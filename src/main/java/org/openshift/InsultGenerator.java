eaf49be918ab4ebb142744e48dc6695edb2a6389
Danny Collins <simpleisbetter@gmail.com>
2020-09-08 19:02:53 +0100

    move to DB

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -1,19 +1,41 @@
 package org.openshift;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Random;
 
 public class InsultGenerator {
-	public String generateInsult() {
-		String words[][] = {{"Artless", "Bawdy", "Beslubbering"}, {"Base-court", "Bat-fowling", "Beef-witted"}, {"Apple-john", "Baggage", "Barnacle"}};
-		String vowels = "AEIOU";
-		String article = "an";
-		String firstAdjective = words[0][new Random().nextInt(words[0].length)];
-		String secondAdjective = words[1][new Random().nextInt(words[1].length)];
-		String noun = words[2][new Random().nextInt(words[2].length)];
-		if (vowels.indexOf(firstAdjective.charAt(0)) == -1) {
-			article = "a";
-		}
-		return String.format("Thou art %s %s %s %s!", article, firstAdjective, secondAdjective, noun);
-	}
+    public String generateInsult() {
+        String vowels = "AEIOU";
+        String article = "an";
+        String theInsult = "";
+
+        String databaseUrl = "jdbc:postgresql://";
+        databaseUrl += System.getenv("POSTGRESQL_SERVICE_HOST");
+        databaseUrl += "/" + System.getenv("POSTGRESQL_DATABASE");
+
+        String userName = System.getenv("POSTGRESQL_USER");
+        String password = System.getenv("PGPASSWORD");
+
+        try (Connection connection = DriverManager.getConnection(databaseUrl, userName, password)) {
+            String SQL = "select a.string as first, b.string as second, c.string as noun from short_adjective a, long_adjective b, nounc c order by random() limit 1";
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(SQL);
+            while (rs.next()) {
+                if (vowels.indexOf(rs.getString("first").charAt(0)) == -1) {
+                    article = "a";
+                }
+                theInsult = String.format("Thou are %s %s %s %s", article, rs.getString("first"), rs.getString("second"), rs.getString("noun"));
+            }
+            rs.close();
+            stmt.close();
+
+        } catch (Exception e) {
+            return "Database Problem!";
+        }
+        return theInsult;
+    }
 
 }
